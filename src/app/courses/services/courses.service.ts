aa16b0cd82ad3928a893d105fd9dd6e6e2832edf
Vinicius Azambuja <vinizambs@gmail.com>
2025-01-02 20:17:12 -0300

    feat - Create separate component for table

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -18,12 +18,12 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API)
     .pipe(
       first(),
-     // delay(5000),
+      delay(5000),
       tap(courses => console.log(courses))
     );
   }
 
-  save(record: Course) {
+  save(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record)
     .pipe(first());
   }
